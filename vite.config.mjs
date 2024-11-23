ebf99c5a00d8f3a6ea83ce834216ed8199284a27
Jozef Izso <jozef.izso@gmail.com>
2024-11-23 16:21:06 +0100

    Change public folder location

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig(async ({ command, mode, isSsrBuild, isPreview }) => 
 
   return {
     root: 'src',
+    publicDir: '../public',
     build: {
       manifest: true,
       outDir: '../dist',
