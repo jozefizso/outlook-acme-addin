f27281341f21ea331e5d9dcba21a4b60a3157a13
Jozef Izso <jozef.izso@gmail.com>
2024-11-23 23:38:40 +0100

    Use bundled Sentry to prevent issues with script blockers

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import './sentry-init'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from "@sentry/browser"
 import Taskpane from './taskpane.jsx'
 
-/* global document, Office, Sentry */
+/* global document, Office */
 
 const container = document.getElementById('root');
 const root = createRoot(container);
